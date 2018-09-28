f219652fae1e255283e82d50018ce1195ac55666
Renny <xhnr@vip.qq.com>
2018-09-28 21:36:37 +0800

    局部组件全局组件使用方法
    Signed-off-by: Renny <xhnr@vip.qq.com>

--- vue/ex/vue-playlist/src/App.vue
+++ vue/ex/vue-playlist/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
+    <user></user>
   </div>
 </template>
 
 <script>
-
+  //局部注册
+  import User from './components/User'
   export default {
     name: 'App',
     data() {
       return {
         title: "this is my first vue=cli"
       }
+    },
+    components: {
+      "user": User
     }
   }
 </script>
--- vue/ex/vue-playlist/src/components/User.vue
+++ vue/ex/vue-playlist/src/components/User.vue
@@ -1,10 +1,19 @@
 <template>
-    
+  <div class="User">
+    <ul>
+      <li v-for="users in (user) ">{{users}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "User"
+    name: "User",
+    data() {
+      return {
+        user: ["Henry", "Bucky", "Emily"]
+      }
+    }
   }
 </script>
 
