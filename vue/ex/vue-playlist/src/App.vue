476b87dc688421534b4e0712f9d1301777a74799
Renny <xhnr@vip.qq.com>
2018-09-28 21:14:19 +0800

    new
    Signed-off-by: Renny <xhnr@vip.qq.com>

--- vue/ex/vue-playlist/src/App.vue
+++ vue/ex/vue-playlist/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        title: "this is my first vue=cli"
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
