e4a29e37b186f6ebefb5865d1dc1a1f61b4ee2f5
Renny <xhnr@vip.qq.com>
2018-09-28 22:47:14 +0800

    完成小demo，组件嵌套
    Signed-off-by: Renny <xhnr@vip.qq.com>

--- vue/ex/vue-playlist/src/App.vue
+++ vue/ex/vue-playlist/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
+    <app-Header></app-Header>
     <user></user>
+    <app-Footer></app-Footer>
   </div>
 </template>
 
 <script>
   //局部注册
+  import Header from './components/Header'
   import User from './components/User'
+  import Footer from './components/Footer'
+
   export default {
     name: 'App',
     data() {
@@ -16,11 +21,15 @@
       }
     },
     components: {
-      "user": User
+      "user": User,
+      "app-Header": Header,
+      "app-Footer": Footer
     }
   }
 </script>
 
-<style>
-
+<style scoped>
+  h1 {
+    color: aquamarine;
+  }
 </style>
--- vue/ex/vue-playlist/src/components/User.vue
+++ vue/ex/vue-playlist/src/components/User.vue
@@ -1,22 +1,58 @@
 <template>
   <div class="User">
+    <h1>Hello Users</h1>
     <ul>
-      <li v-for="users in (user) ">{{users}}</li>
+      <li v-for="user in users " v-on:click="user.show=!user.show"><h2>{{user.name}}</h2>
+        <h3 v-show="user.show">{{user.position}}</h3>
+      </li>
+
     </ul>
+
   </div>
 </template>
 
 <script>
   export default {
-    name: "User",
+    name: "Users",
     data() {
       return {
-        user: ["Henry", "Bucky", "Emily"]
+        users: [
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false},
+          {name: "Tom", position: "前端", show: false}
+        ]
       }
     }
   }
 </script>
 
 <style scoped>
+  .User {
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    box-sizing: border-box;
+  }
 
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    flex-grow: 1;
+    flex-basis: 200px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+  }
 </style>
