67fad5ffbfaed27f2b60ee6ed52c996e0bc95ec6
Renny <xhnr@vip.qq.com>
2018-10-19 09:51:28 +0800

    ms

---
+++ js/m.md.js
@@ -0,0 +1,29 @@
+/* 1\js原型链
+2、封包闭包
+3、数据结构
+4、排序
+5.console.log() 
+6.比较大小
+7.bootsterap
+8.css基本用法
+
+
+
+*/
+
+var a=123;
+var b=function(){
+    a+=1;
+    return a;
+
+}
+console.log(a);
+function c(){
+   return b+a;
+   
+
+} ;
+console.log(c);
+
+
+console.log(this);
