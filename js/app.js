a0af367760f2807e9ec5e4f951f2b7ceb0b44b31
Renny <xhnr@vip.qq.com>
2018-10-21 18:30:08 +0800

    不记得上次做了什么更新了=====索性先提交一遍……

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 var a=1+2+3
 console.log(a)
 confirm("234");
+
+console.log("---------");
+
+
+
+function hello() {
+    var a = 10;
+    alert(hello);
+}
+
+console.log(hello);
+
+var message;
+console.log(typeof message);
--- js/深入理解JavaScript原型和闭包/app.js
+++ js/深入理解JavaScript原型和闭包/app.js
@@ -7,3 +7,12 @@ Fn.prototype.getYear=function(){
 var fn=new Fn();
 console.log(fn.name);
 console.log(fn.getYear());
+
+console.log("---------");
+
+
+
+function hello(){
+    var a=10;
+    alert(hello);
+}
