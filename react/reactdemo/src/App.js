93fb89e50852917ac38ecc5d6af07150de6b8d80
Renny <xhnr@vip.qq.com>
2018-10-24 13:52:16 +0800

    组建的用法

--- react/reactdemo/src/App.js
+++ react/reactdemo/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person  from "./Person/Person";
 class App extends Component {
   render() {
-  //   return (
-  //     <div className="App">
-  //       <h1>hello</h1>
-  //     </div>
-  //   );
-  return React.createElement('div',{className:'App'},'h1','helloworld');
+    return (
+      <div className="App">
+        <h1>hello</h1>
+        <Person/>
+        <Person />
+        <Person />
+        <Person />
+
+      </div>
+    );
+  // return React.createElement('div',{className:'App'},React.createElement('h1',null,'hello') );
    }
   }
 
---
+++ react/reactdemo/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React  from 'react';
+
+
+const person= ()=>{
+return <p>hello joynop</p>
+}
+
+export default person;
