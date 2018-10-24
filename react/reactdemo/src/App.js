93a600da2ae888bb0b65e68b6ae48a74bca26d19
Renny <xhnr@vip.qq.com>
2018-10-24 13:40:08 +0800

    直接生成连接

--- react/reactdemo/src/App.js
+++ react/reactdemo/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>hello</h1>
-      </div>
-    );
+  //   return (
+  //     <div className="App">
+  //       <h1>hello</h1>
+  //     </div>
+  //   );
+  return React.createElement('div',{className:'App'},'h1','helloworld');
+   }
   }
-}
 
 export default App;
